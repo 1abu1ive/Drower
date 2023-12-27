65c42e8be6e8b0b4f926cb04b191315af962490e
SizningIsmingiz <github691@gmail.com>
2023-12-27 20:05:24 +0500

    first commit

--- src/pages/api/students.js
+++ src/pages/api/students.js
@@ -1,5 +1,7 @@
+import { runMiddleware } from "../../../backand/middlewares";
 import students from "../../../data/students";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  await runMiddleware(req, res);
   res.status(200).json(students);
 }
